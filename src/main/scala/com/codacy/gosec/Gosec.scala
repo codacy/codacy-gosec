7875812e5aecd69eee55072263b5b486310d6fc7
José Miguel  Melo <josemiguelmelo94@gmail.com>
2020-02-18 17:11:11 +0000

    Implement gosec transformer

--- src/main/scala/com/codacy/gosec/Gosec.scala
+++ src/main/scala/com/codacy/gosec/Gosec.scala
@@ -1,8 +1,37 @@
 package com.codacy.gosec
 
 import com.codacy.analysis.core.model.IssuesAnalysis
+import com.codacy.analysis.core.model.IssuesAnalysis.FileResults
 import com.codacy.tool.ClientSideToolEngine
 
+import scala.util.{Failure, Success, Try}
+
 object Gosec extends ClientSideToolEngine(toolName = "gosec") {
-  override def convert(lines: Seq[String]): IssuesAnalysis = ???
+  def gosecReportToFileResults(report: GosecResult): Try[Set[FileResults]] = {
+    Try {
+      report.issues
+        .groupBy(_.file)
+        .view
+        .map {
+          case (path, res) =>
+            FileResults(path, res.view.map(_.toCodacyIssue).toSet)
+        }
+        .to(Set)
+    }
+  }
+
+  def gosecReportToIssuesAnalysis(report: GosecResult): IssuesAnalysis = {
+    gosecReportToFileResults(report) match {
+      case Success(result) => IssuesAnalysis.Success(result)
+      case Failure(exception) => IssuesAnalysis.Failure(exception.getMessage)
+    }
+
+  }
+
+  override def convert(lines: Seq[String]): IssuesAnalysis = {
+    GosecReportParser.fromJson(lines) match {
+      case Success(report) => gosecReportToIssuesAnalysis(report)
+      case Failure(err) => IssuesAnalysis.Failure(err.getMessage)
+    }
+  }
 }
---
+++ src/test/scala/com/codacy/gosec/GosecSpec.scala
@@ -0,0 +1,57 @@
+package com.codacy.gosec
+
+import com.codacy.analysis.core.model.IssuesAnalysis.{Failure, Success}
+import org.scalatest.TryValues._
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class GosecSpec extends AnyFlatSpec with Matchers {
+
+  "GoSec" should "parse correctly" in {
+    val result = Gosec.convert(Seq(CommonTestMock.resultJsonText))
+
+    val expectedResult = Success(Set(CommonTestMock.fileResults))
+
+    assert(result == expectedResult)
+  }
+
+  it should "fail when invalid format given" in {
+    val text = Seq("""<test><xmlformat></xmlformat></test>""")
+    val result = Gosec.convert(text)
+    assert(result.isInstanceOf[Failure])
+  }
+
+  it should "fail the parse when json with missing information is given" in {
+    val lines = Seq(
+      s"""{
+         |    "Issues": [
+         |        {
+         |            "severity": "LOW"
+         |        }
+         |    ]
+         |}""".stripMargin)
+
+    val result = Gosec.convert(lines)
+
+    assert(result.isInstanceOf[Failure])
+  }
+
+  it should "convert gosec report into codacy file results report" in {
+    val fileResults = Gosec.gosecReportToFileResults(CommonTestMock.resultAsGosecResult)
+    assert(fileResults.success.value == Set(CommonTestMock.fileResults))
+  }
+
+  it should "throw exception on null param" in {
+    assert(Gosec.gosecReportToFileResults(null).isFailure)
+  }
+
+  it should "return success" in {
+    val issuesAnalysis = Gosec.gosecReportToIssuesAnalysis(CommonTestMock.resultAsGosecResult)
+    assert(issuesAnalysis.isInstanceOf[Success])
+  }
+
+  it should "return failure" in {
+    val issuesAnalysis = Gosec.gosecReportToIssuesAnalysis(null)
+    assert(issuesAnalysis.isInstanceOf[Failure])
+  }
+}
