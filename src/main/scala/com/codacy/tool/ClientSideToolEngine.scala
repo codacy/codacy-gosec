c667a80468b0f6b67e3002a4894fb16a4ab6f7ac
José Miguel  Melo <josemiguelmelo94@gmail.com>
2020-02-18 17:35:58 +0000

    Serialize tool result

--- src/main/scala/com/codacy/tool/ClientSideToolEngine.scala
+++ src/main/scala/com/codacy/tool/ClientSideToolEngine.scala
@@ -1,9 +1,14 @@
 package com.codacy.tool
 
+import com.codacy.analysis.core.model.ToolResults
+import com.codacy.analysis.core.serializer.IssuesReportSerializer
+
 abstract class ClientSideToolEngine(toolName: String) extends IssuesAnalysisConverter {
   def main(args: Array[String]): Unit = {
     val lines = scala.io.Source.stdin.getLines().to(LazyList)
-    val jsonResult = convert(lines)
+    val issuesResults = convert(lines)
+    val toolResults = ToolResults(this.toolName, issuesResults)
+    val jsonResult = IssuesReportSerializer.toJsonString(Set(toolResults))
     println(jsonResult)
   }
 }
