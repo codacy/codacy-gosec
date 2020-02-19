4a9f466f7e53fcc828df4227eec8155dc9e8dffa
José Miguel  Melo <josemiguelmelo94@gmail.com>
2020-02-19 11:46:35 +0000

    Add run method to issues analysis converter

--- src/main/scala/com/codacy/tool/ClientSideToolEngine.scala
+++ src/main/scala/com/codacy/tool/ClientSideToolEngine.scala
@@ -1,15 +1,8 @@
 package com.codacy.tool
 
-import com.codacy.analysis.core.model.ToolResults
-import com.codacy.analysis.core.serializer.IssuesReportSerializer
-
 abstract class ClientSideToolEngine(toolName: String) extends IssuesAnalysisConverter {
 
   def main(args: Array[String]): Unit = {
-    val lines = scala.io.Source.stdin.getLines().to(LazyList)
-    val issuesResults = convert(lines)
-    val toolResults = ToolResults(this.toolName, issuesResults)
-    val jsonResult = IssuesReportSerializer.toJsonString(Set(toolResults))
-    println(jsonResult)
+    run(this.toolName)
   }
 }
